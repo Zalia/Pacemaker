ef75e38e2886bab83b8d5c6a6667ea2d8628a6ba
Zalia <t.schueller@online.de>
2017-02-15 23:25:16 +0100

    modified layout to work hierarchical, implemented layout/mode switching for three modes (color picker mode is still a bit buggy)

--- app/src/main/java/zalia/pacemaker/MainActivity.java
+++ app/src/main/java/zalia/pacemaker/MainActivity.java
@@ -3,14 +3,21 @@ package zalia.pacemaker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Toast;
+import android.widget.Spinner;
 
 import com.flask.colorpicker.ColorPickerView;
 import com.flask.colorpicker.OnColorSelectedListener;
 
 public class MainActivity extends AppCompatActivity {
     private View root;
+    private View modeView;
     private int currentBackgroundColor = 0xffffffff;
+
+    private Spinner spinner;
+    private ColorPickerView colorPickerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +26,51 @@ public class MainActivity extends AppCompatActivity {
         root = findViewById(R.id.color_screen);
         changeBackgroundColor(currentBackgroundColor);
 
-        ColorPickerView colorPickerView = (ColorPickerView) findViewById(R.id.color_picker_view);
-        colorPickerView.addOnColorSelectedListener(new OnColorSelectedListener() {
+        spinner = (Spinner) findViewById(R.id.modes_dropdown);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onColorSelected(int selectedColor) {
-                Toast.makeText(
-                        MainActivity.this,
-                        "selectedColor: " + Integer.toHexString(selectedColor).toUpperCase(),
-                        Toast.LENGTH_SHORT).show();
-                changeBackgroundColor(selectedColor);
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                ViewGroup modeViewHolder = (ViewGroup)findViewById(R.id.frame_layout);
+
+                toast("handling selected item");
+                if(modeView != null){
+                    modeViewHolder.removeView(modeView);
+                }
+
+                int modeViewResId;
+                switch(position){
+                    case 0: default:
+                        modeViewResId = R.layout.effect_layout;
+                        break;
+                    case 1:
+                        modeViewResId = R.layout.full_rainbow_layout;
+                        break;
+                    case 2:
+                        modeViewResId = R.layout.color_picker_layout;
+                        break;
+                }
+
+                modeView = getLayoutInflater().inflate(modeViewResId, null);
+                modeViewHolder.addView(modeView);
+                if(position == 2){
+                    toast("attempting to initialize color picker");
+                    colorPickerView = (ColorPickerView) findViewById(R.id.color_picker_view);
+                    colorPickerView.addOnColorSelectedListener(new OnColorSelectedListener() {
+                        @Override
+                        public void onColorSelected(int selectedColor) {
+                            Toast.makeText(
+                                    MainActivity.this,
+                                    "selectedColor: " + Integer.toHexString(selectedColor).toUpperCase(),
+                                    Toast.LENGTH_SHORT).show();
+                            changeBackgroundColor(selectedColor);
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
             }
         });
 
@@ -41,4 +84,6 @@ public class MainActivity extends AppCompatActivity {
     private void toast(String text) {
         Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
     }
+
+
 }
