605e1449021ee5e48337e38256f5385ba2f2fe70
Zalia <t.schueller@online.de>
2017-02-19 01:08:52 +0100

    improved visibility, added new icon, improved bt error handling

--- app/src/main/java/zalia/pacemaker/MainActivity.java
+++ app/src/main/java/zalia/pacemaker/MainActivity.java
@@ -74,16 +74,6 @@ public class MainActivity extends AppCompatActivity {
         //setup bluetooth connection
         btAdapter = BluetoothAdapter.getDefaultAdapter();
         checkBTState();
-
-        //setup commit button
-        Button commit_button = (Button) findViewById(R.id.commit_button);
-        commit_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                send_config(active_mode.generate_configs());
-            }
-        });
-
     }
 
     /*
@@ -93,7 +83,55 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onResume() {
         super.onResume();
+        connectBt();
+    }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        try{
+            Log.d(TAG, "...In onPause()...");
+
+            if (outStream != null) {
+                try {
+                    outStream.flush();
+                } catch (IOException e) {
+                    errorExit("Fatal Error", "In onPause() and failed to flush output stream: " + e.getMessage() + ".");
+                }
+            }
+
+            try {
+                btSocket.close();
+            } catch (IOException e2) {
+                errorExit("Fatal Error", "In onPause() and failed to close socket." + e2.getMessage() + ".");
+            }
+        } catch(NullPointerException e){
+            errorExit("Fatal Error", "NullPointer in onPause(): Bluetooth connection broken");
+            disableCommit();
+        }
+    }
+
+    private void checkBTState() {
+        // Check for Bluetooth support and then check to make sure it is turned on
+
+        // Emulator doesn't support Bluetooth and will return null
+        if(btAdapter==null) {
+            disableCommit();
+            errorExit("Fatal Error", "Bluetooth Not supported. Aborting.");
+        } else {
+            if (btAdapter.isEnabled()) {
+                Log.d(TAG, " ...Bluetooth is enabled...");
+            } else {
+                //Prompt user to turn on Bluetooth
+                Intent enableBtIntent = new Intent(btAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, 1);
+            }
+            enableCommit();
+        }
+    }
+
+    private void connectBt(){
         try {
             Log.d(TAG, "...In onResume - Attempting client connect...");
 
@@ -136,50 +174,32 @@ public class MainActivity extends AppCompatActivity {
                 errorExit("Fatal Error", "In onResume() and output stream creation failed:" + e.getMessage() + ".");
             }
         }catch(NullPointerException e){
-            errorExit("Fatal Error", "NullPointer in onResume(): Bluetooth not active or not supported");
+            errorExit("Warning", "NullPointer in onResume(): Bluetooth not active/supported or connection broken");
+            disableCommit();
         }
     }
 
-    @Override
-    public void onPause() {
-        super.onPause();
-
-        try{
-            Log.d(TAG, "...In onPause()...");
-
-            if (outStream != null) {
-                try {
-                    outStream.flush();
-                } catch (IOException e) {
-                    errorExit("Fatal Error", "In onPause() and failed to flush output stream: " + e.getMessage() + ".");
-                }
+    private void disableCommit(){
+        //setup commit button
+        Button commit_button = (Button) findViewById(R.id.commit_button);
+        commit_button.setText(R.string.connect);
+        commit_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                connectBt();
             }
-
-            try {
-                btSocket.close();
-            } catch (IOException e2) {
-                errorExit("Fatal Error", "In onPause() and failed to close socket." + e2.getMessage() + ".");
-            }
-        } catch(NullPointerException e){
-            errorExit("Fatal Error", "NullPointer in onPause(): Bluetooth not active or not supported");
-        }
+        });
     }
 
-    private void checkBTState() {
-        // Check for Bluetooth support and then check to make sure it is turned on
-
-        // Emulator doesn't support Bluetooth and will return null
-        if(btAdapter==null) {
-            errorExit("Fatal Error", "Bluetooth Not supported. Aborting.");
-        } else {
-            if (btAdapter.isEnabled()) {
-                Log.d(TAG, " ...Bluetooth is enabled...");
-            } else {
-                //Prompt user to turn on Bluetooth
-                Intent enableBtIntent = new Intent(btAdapter.ACTION_REQUEST_ENABLE);
-                startActivityForResult(enableBtIntent, 1);
+    private void enableCommit(){
+        Button commit_button = (Button) findViewById(R.id.commit_button);
+        commit_button.setText(R.string.commit);
+        commit_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                send_config(active_mode.generate_configs());
             }
-        }
+        });
     }
 
     private void send_config(String config){
